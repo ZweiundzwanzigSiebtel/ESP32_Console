2f4c5e69c12d74de6e2a1e0cbe97e51817e8386f
Silas Ruffner <silas@MacBook-Pro-3.local>
2023-04-05 20:34:33 +0200

    it seems like a framebuffer[320*240*2] is too large to hold in RAM

--- include/devices/spi_display.h
+++ include/devices/spi_display.h
@@ -2,21 +2,15 @@
 #define SCREEN_H
 #include "driver/spi_master.h"
 
-#include "abstract_display.h"
-
 class SpiDisplay {
    public:
     SpiDisplay();
     //~SpiDisplay() override;
     void update_screen();
 
-    void draw_pixel(uint16_t x, uint16_t y, uint16_t color);
-    void draw_line(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, uint16_t color);
-
    private:
     spi_device_handle_t spi;
-    uint8_t frame_buffer[320 * 240 * 2];
-    void init();
+    // uint8_t frame_buffer[320 * 240 * 2] = {0};
     void send_block(uint8_t* linedata);
     spi_device_handle_t begin();
     void lcd_init();
